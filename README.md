3da565172aa3a5e6e9b3c9f68f83e6ed58cda035
ikimodus <ifhumail@gmail.com>
2026-04-19 21:38:33 +0200

    playRound logic completed Fixed 2 glitches in getChoice: - Added "()" to .toLowerCase - Added "choice == number" Update to ReadMe

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ My Rock Papers Scissors project being tutored by Foundations, Odin Project.
 
 Logic:
 Game start >
-    Prompt human (Rock, Paper, Scissors) >
-        Bot choice = randomNum (1-3)
-        
+    Bot choice = randomNum (1-3) > Rock || Paper || Scissors
+    Prompt human = Rock || Paper || Scissors
+    
+    If human has the same as computer then
+    Output "Draw"
+    If (human has rock and computer has scissors) OR (human has paper and computer has rock) OR (human has scissors and computer has paper) then
+    Output "human wins"
+    Else
+    Output "computer wins"
+
+    Output Scores points
+
+    
     
--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let computerScore = 0
 let humanScore = 0
 
 function getChoice(choice) {
-    if (choice == "rock" || 1)
+    if (choice == "rock" || choice == 1)
         return "Rock"
-    else if (choice == "paper" || 2)
+    else if (choice == "paper" || choice == 2)
         return "Paper"
     else
         return "Scissors"
@@ -17,13 +17,37 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("hi")
-    return getChoice(choice.toLowerCase)
+    return getChoice(choice.toLowerCase())
 }
 
-function playRound() {
-    let humanChoice = getHumanChoice()
-    let computerChoice = getComputerChoice()
-    console.log(computerChoice + " " + humanChoice)
+function playRound(humanChoice, computerChoice) {
+    let outcome
+
+    if (humanChoice == computerChoice)
+        outcome = "draw"
+    else if (humanChoice == "Rock" && computerChoice == "Scissors" ||
+        humanChoice == "Paper" && computerChoice == "Rock" || 
+        humanChoice == "Scissors" && computerChoice == "Paper")
+        outcome = "win"
+    else
+        outcome = "lose"
+
+    if (outcome == "draw")
+        console.log(`Draw! You both played ${humanChoice} and ${computerChoice}. No points awarded.`)
+    else if (outcome == "win") {
+        humanScore += 1
+        console.log(`You win! ${humanChoice} beats ${computerChoice}. +1 Point awarded to you.`)
+    }
+    else {
+        computerScore += 1
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}. +1 Point awarded to computer.`)
+    }
+
+    console.log(`Current Score: `)
+    console.log(`You: ${humanScore}`)
+    console.log(`Computer: ${computerScore}`)
 }
 
-playRound()
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+playRound(humanSelection, computerSelection)
