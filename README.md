fe1dd3458e13e86737f439cd9bb4b15fa34eb251
ikimodus <ifhumail@gmail.com>
2026-04-19 20:37:49 +0200

    Implemented humanChoice and computerChoice logic

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # odin-rock-paper-scissors
 My Rock Papers Scissors project being tutored by Foundations, Odin Project.
+
+
+Logic:
+Game start >
+    Prompt human (Rock, Paper, Scissors) >
+        Bot choice = randomNum (1-3)
+        
+    
--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hello')
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1
+    console.log("computer choice : " + computerChoice)
+    if (computerChoice === 1)
+        return "Rock";
+    else if (computerChoice === 2)
+        return "Paper";
+    else 
+        return "Scissors";
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("hi")
+    console.log("human choice : " + humanChoice)
+
+    if (humanChoice == "rock" || 1)
+        return "Rock"
+    else if (humanChoice == "paper" || 2)
+        return "Paper"
+    else
+        return "Scissors"
+}
+
+getComputerChoice()
+getHumanChoice()
