3042135f61ef7ddcb19a3cf3c49d818b075c44d8
ikimodus <ifhumail@gmail.com>
2026-05-24 04:37:45 +0200

    Console RPS game to page interactive RPS - Removed max rounds - Added max points - Added basic ui

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
-let computerScore = 0
-let humanScore = 0
-let rounds = 0
-const MAX_ROUNDS = 5
+let computerScore = 0;
+let humanScore = 0;
+let rounds = 0;
+const MAX_ROUNDS = 0;
+const MAX_POINTS = 5;
+
+let rockBtn = document.querySelector(".rock-btn");
+let paperBtn = document.querySelector(".paper-btn");
+let scissorsBtn = document.querySelector(".scissors-btn");
+let results = document.querySelector(".results");
+let statusText = document.querySelector(".status-text");
 
 function getChoice(choice) {
-    if (choice == "rock" || choice == 1)
-        return "Rock"
-    else if (choice == "paper" || choice == 2)
-        return "Paper"
-    else
-        return "Scissors"
+  if (choice == "rock" || choice == 1) return "Rock";
+  else if (choice == "paper" || choice == 2) return "Paper";
+  else return "Scissors";
 }
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3) + 1
-    return getChoice(choice)
+  let choice = Math.floor(Math.random() * 3) + 1;
+  return getChoice(choice);
 }
 
-function getHumanChoice() {
-    let choice = prompt(`Rock Paper Scissors... Shoot! (${rounds}/${MAX_ROUNDS})`)
-    return getChoice(choice.toLowerCase())
-}
+function playRound(humanChoice) {
+  const computerChoice = getComputerChoice();
+  let outcome;
 
-function playRound() {
-    const humanChoice = getHumanChoice()
-    const computerChoice = getComputerChoice()
-    let outcome
+  if (humanChoice == computerChoice) outcome = "draw";
+  else if (
+    (humanChoice == "Rock" && computerChoice == "Scissors") ||
+    (humanChoice == "Paper" && computerChoice == "Rock") ||
+    (humanChoice == "Scissors" && computerChoice == "Paper")
+  )
+    outcome = "win";
+  else outcome = "lose";
 
-    if (humanChoice == computerChoice)
-        outcome = "draw"
-    else if (humanChoice == "Rock" && computerChoice == "Scissors" ||
-        humanChoice == "Paper" && computerChoice == "Rock" || 
-        humanChoice == "Scissors" && computerChoice == "Paper")
-        outcome = "win"
-    else
-        outcome = "lose"
+  if (outcome == "draw")
+    results.textContent = `Draw! You both played ${humanChoice} and ${computerChoice}. No points awarded.`;
+  else if (outcome == "win") {
+    humanScore += 1;
+    results.textContent = `You win! ${humanChoice} beats ${computerChoice}. +1 Point awarded to you.`;
+  } else {
+    computerScore += 1;
+    results.textContent = `You lose! ${computerChoice} beats ${humanChoice}. +1 Point awarded to computer.`;
+  }
 
-    if (outcome == "draw")
-        console.log(`Draw! You both played ${humanChoice} and ${computerChoice}. No points awarded.`)
-    else if (outcome == "win") {
-        humanScore += 1
-        console.log(`You win! ${humanChoice} beats ${computerChoice}. +1 Point awarded to you.`)
-    }
-    else {
-        computerScore += 1
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}. +1 Point awarded to computer.`)
-    }
+  statusText.innerHTML = `Current Score:</br>
+                          You: ${humanScore}</br>
+                          Computer: ${computerScore}
+                          `;
 
-    console.log(`Current Score: `)
-    console.log(`You: ${humanScore}`)
-    console.log(`Computer: ${computerScore}`)
-    console.log("---------------------------------")
+  if (humanScore >= MAX_POINTS || computerScore >= MAX_POINTS) {
+    if (humanScore == computerScore)
+      alert("Too bad! The game ended in a DRAW.");
+    else if (humanScore > computerScore) alert("Congratulations! You WIN.");
+    else alert("Too bad! You LOST.");
+
+    location.reload();
+  }
 }
 
 function playGame() {
-    console.log('Game Started.')
-    for (rounds = 1; rounds <= MAX_ROUNDS; rounds++ ) {
-        playRound()
-    }
-
-    if (humanScore == computerScore)
-        console.log("Too bad! The game ended in a DRAW.")
-    else if (humanScore > computerScore)
-        console.log("Congratulations! You WIN.")
-    else
-        console.log("Too bad! You LOST.")
-    console.log("Feel free to Refresh the page to play again!")
+  console.log("Game Started.");
+  rockBtn.addEventListener("click", () => playRound("Rock"));
+  paperBtn.addEventListener("click", () => playRound("Paper"));
+  scissorsBtn.addEventListener("click", () => playRound("Scissors"));
 }
 
-playGame()
+playGame();
